3718fcd382509416652ff9179c280c9a0f52a069
Shiva953 <b22070@students.iitmandi.ac.in>
2025-02-02 17:53:47 +0530

    ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Mail, Github, Twitter } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { ChevronRight } from 'lucide-react'
 import localFont from 'next/font/local'
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
+import {Roboto_Mono} from "next/font/google"
+
+const roboto = Roboto_Mono({style: ["normal"], subsets: ["latin", "latin-ext"]})
 
 const supply = localFont({
   src: './font/Supply-Regular.otf',
@@ -16,7 +23,7 @@ const montreal = localFont({
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <div className="max-w-6xl mx-auto px-6 py-6 space-y-24">
+      <div className="max-w-4xl mx-auto px-6 py-6 space-y-12">
         {/* Header */}
         <header className="flex justify-between items-start">
           <h1 className={` text-3xl font-bold text-white ${supply.className}`}>SHIVA</h1>
@@ -34,103 +41,201 @@ export default function Home() {
         </header>
 
         {/* Hero */}
-        <section className={`space-y-4 tracking-wide ${montreal.className}`}>
-          <p className="text-[1.12rem] text-white/75 max-w-3xl">
-            Full-Stack Solana Engineer and a self-taught developer. Won{' '}
-            <span className='text-white underline hover:cursor-pointer'>2 Hackathons</span> previously, also a{' '}
-            <span className='text-white underline hover:cursor-pointer'>Solana Foundation</span> Grantee. Love to build, contribute to <span className='text-white underline hover:cursor-pointer'>open source</span> occasionally.
+        <section className={`space-y-4 ${montreal.className}`}>
+          <p className="text-[1rem] tracking-[0.0175rem] text-white/60 max-w-3xl">
+            Hi, I{"'"}m Shiva. I{"'"}m a full-stack solana engineer and a self-taught developer. Won{' '}
+            <span className='text-white font-semibold hover:cursor-pointer'>2 Hackathons</span> previously, also a{' '}
+            <span className='text-white font-semibold hover:cursor-pointer'>Solana Foundation</span> Grantee. Love to build, contribute to <span className='text-white font-semibold hover:cursor-pointer'>open source</span> occasionally.
           </p>
-          <p className="text-[1.12rem] text-white/70">
-            You can find me on <span className='text-white underline hover:cursor-pointer'>github</span>, or DM me on <span className='text-white underline hover:cursor-pointer'>X</span>.
+          <p className="text-[1rem] tracking-[0.0175rem] text-white/70">
+            I like nerding about cryptography and the math behind DeFi. You can find me on <span className='text-white underline hover:cursor-pointer'><Link href={"https://github.com/Shiva953"}>github</Link></span>, or DM me on <span className='text-white underline hover:cursor-pointer'><Link href={"https://x.com/Neutron975"}>X</Link></span>.
           </p>
-          <p className="text-[1.25rem] text-white font-semibold">
+          <p className="text-[1rem] tracking-[0.0175rem] text-white font-extrabold">
             Member at @<span className='underline hover:cursor-pointer'>SuperteamIN</span>
           </p>
         </section>
 
+        <h2 className={`text-2xl text-white font-extrabold tracking-wide mb--8 ${montreal.className}`}>Work</h2>
+
+      {/* Experience */}
+      <section className="space-y-2 text-sm tracking-wide">
+        <div className="space-y-6">
+          <div className="flex items-center gap-4">
+            <div className="relative h-12 w-12 overflow-hidden rounded-full">
+              <Image
+                src="/STINDIA.jpg"
+                alt="Superteam"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="flex-1">
+              <div className={`flex items-center justify-between ${montreal.className} cursor-pointer`}>
+                <div>
+                <Link href={"https://superteam.fun"}>
+                  <div className="group flex items-center gap-2">
+                    <h3 className="font-bold text-white">Superteam</h3>
+                    <ChevronRight className="h-4 w-4 transform opacity-0 transition-all duration-300 ease-in-out group-hover:translate-x-1 group-hover:opacity-100 text-white" />
+                  </div>
+                  </Link>
+                  
+                  <p className="text-white/60">
+                    Member, Contributing to the Solana Ecosystem
+                  </p>
+                </div>
+                <span className="text-gray-400">January 2025 - Present</span>
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center gap-4 cursor-pointer">
+            <div className="relative h-12 w-12 overflow-hidden rounded-full">
+              <Image src="/earn.jpg" alt="freelance" fill className="object-cover" />
+            </div>
+            <div className="flex-1">
+              <div className={`flex items-center justify-between ${montreal.className}`}>
+                <div>
+                <Link href={"https://earn.superteam.fun/t/shiva7343/"}>
+                  <div className="group flex items-center gap-2">
+                    <h3 className="font-bold text-white">Freelance | Superteam Earn & Gibwork</h3>
+                    <ChevronRight className="h-4 w-4 transform opacity-0 transition-all duration-300 ease-in-out group-hover:translate-x-1 group-hover:opacity-100 text-white" />
+                  </div>
+                  </Link>
+                  <p className="text-white/60">Got numerous gigs from platforms like Superteam Earn and Gibwork</p>
+                </div>
+                <span className="text-gray-400">July 2024 - Present</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+      <h2 className={`text-2xl text-white font-extrabold tracking-wide ${montreal.className}`}>Projects</h2>
+
         {/* Projects */}
         <section className="space-y-2">
           <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center gap-16">
-            <Link href="/projects/belzin" className="group w-full max-w-xl">
+            <Link href="/projects/belzin" className="group w-full max-w-[30rem]">
               <div className="space-y-4">
-                <div className="relative w-full h-[360px] overflow-hidden rounded-lg">
+                <div className="relative w-full h-[250px] overflow-hidden rounded-lg">
                   <Image
-                    src=""
-                    alt="Sandbox Project"
+                    src="/belzin.png"
+                    alt="Belzin"
                     fill
-                    className="object-cover image-glow"
+                    className="object-cover image-glow opacity-80 hover:opacity-100 transition-opacity"
                   />
                 </div>
                 <div className="space-y-1">
                   <div className="flex items-center justify-between">
-                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${supply.className}`}>BELZIN</h3>
+                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${montreal.className}`}>Belzin</h3>
                     <span className="text-gray-400">2025</span>
                   </div>
-                  <p className={`opacity-65 font-medium ${montreal.className}`}>
-                    AI-Agent Powered P2P Group Chat Betting
+                  <p className={`text-white/65 tracking-tight font-[400] text-[0.8rem] ${roboto.className}`}>
+                    AI-Agent Powered P2P Group Chat Betting. Received a <span className='text-white/90'>$3000 grant</span> from the <span className="text-white/90">Solana Foundation</span>.
                   </p>
                 </div>
               </div>
             </Link>
-            <Link href="/projects/univault" className="group w-full max-w-xl">
+            <Link href="/projects/univault" className="group w-full max-w-[30rem]">
               <div className="space-y-4">
-                <div className="relative w-full h-[360px] overflow-hidden rounded-lg">
+                <div className="relative w-full h-[250px] overflow-hidden rounded-lg">
                   <Image
-                    src=""
-                    alt="Arceus Project"
+                    src="/univault.png"
+                    alt="Univault"
                     fill
-                    className="object-cover image-glow"
+                    className="object-cover image-glow opacity-80 hover:opacity-100 transition-opacity"
                   />
                 </div>
                 <div className="space-y-1">
                   <div className="flex items-center justify-between">
-                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${supply.className}`}>UNIVAULT</h3>
+                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${montreal.className}`}>Univault</h3>
                     <span className="text-gray-400">2024</span>
                   </div>
-                  <p className={`opacity-65 font-medium ${montreal.className}`}>
-                    Squads Multisig Management using Solana Blinks
+                  <p className={`text-white/65 tracking-tight font-[400] text-[0.8rem] ${roboto.className}`}>
+                    Squads Multi-sig Management using Solana Blinks and Actions
                   </p>
                 </div>
               </div>
             </Link>
-            <Link href="/projects/liquotic" className="group w-full max-w-xl">
+            <Link href="https://github.com/pulls?q=is%3Amerged+is%3Apr+author%3AShiva953+archived%3Afalse+" className="group w-full max-w-[30rem]">
               <div className="space-y-4">
-                <div className="relative w-full h-[360px] overflow-hidden rounded-lg">
+                <div className="relative w-full h-[250px] overflow-hidden rounded-lg">
                   <Image
-                    src=""
-                    alt="Sandbox Project"
+                    src="/oss.png"
+                    alt="OSS"
                     fill
-                    className="object-cover image-glow"
+                    className="object-cover image-glow opacity-80 hover:opacity-100 transition-opacity"
                   />
                 </div>
                 <div className="space-y-1">
                   <div className="flex items-center justify-between">
-                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${supply.className}`}>LIQUOTIC</h3>
+                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${montreal.className}`}>OSS Contributions</h3>
+                    <span className="text-gray-400">2024-25</span>
+                  </div>
+                  <p className={`text-white/65 tracking-tight font-[400] text-[0.8rem] ${roboto.className}`}>
+                    Some of my merged PRs in open source repositories, including the official anchor repo
+                  </p>
+                </div>
+              </div>
+            </Link>
+            <Link href="/projects/liquotic" className="group w-full max-w-[30rem]">
+              <div className="space-y-4">
+                <div className="relative w-full h-[250px] overflow-hidden rounded-lg">
+                  <Image
+                    src="/liquotic.jpg"
+                    alt="Liquotic"
+                    fill
+                    className="object-cover image-glow opacity-80 hover:opacity-100 transition-opacity"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <div className="flex items-center justify-between">
+                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${montreal.className}`}>Liquotic</h3>
                     <span className="text-gray-400">2024</span>
                   </div>
-                  <p className={`opacity-65 font-medium ${montreal.className}`}>
+                  <p className={`text-white/65 tracking-tight font-[400] text-[0.8rem] ${roboto.className}`}>
                     Buy NFTs with any SPL-Token
                   </p>
                 </div>
               </div>
             </Link>
-            <Link href="/projects/openvest" className="group w-full max-w-xl">
+            <Link href="/projects/openvest" className="group w-full max-w-[30rem]">
               <div className="space-y-4">
-                <div className="relative w-full h-[360px] overflow-hidden rounded-lg">
+                <div className="relative w-full h-[250px] overflow-hidden rounded-lg">
                   <Image
-                    src=""
-                    alt="Arceus Project"
+                    src="/openvest.png"
+                    alt="OpenVest"
                     fill
-                    className="object-cover image-glow"
+                    className="object-cover image-glow opacity-80 hover:opacity-100 transition-opacity"
                   />
                 </div>
                 <div className="space-y-1">
                   <div className="flex items-center justify-between">
-                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${supply.className}`}>OPENVEST</h3>
+                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${montreal.className}`}>OpenVest</h3>
                     <span className="text-gray-400">2024</span>
                   </div>
-                  <p className={`opacity-65 font-medium ${montreal.className}`}>
+                  <p className={`text-white/65 tracking-tight font-[400] text-[0.8rem] ${roboto.className}`}>
                     Create Token Vesting Schedules for Company Employees
+                  </p>
+                </div>
+              </div>
+            </Link>
+            <Link href="https://github.com/Shiva953/sol-clix" className="group w-full max-w-[30rem]">
+              <div className="space-y-4">
+                <div className="relative w-full h-[250px] overflow-hidden rounded-lg">
+                  <Image
+                    src="/solclix.png"
+                    alt="solclix"
+                    fill
+                    className="object-cover image-glow opacity-80 hover:opacity-100 transition-opacity"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <div className="flex items-center justify-between">
+                    <h3 className={`text-xl text-white font-bold group-hover:text-white group-hover:underline group-hover:drop-shadow-[0_0_6px_rgba(255,255,255,0.4)] ${montreal.className}`}>SOL-CLIX</h3>
+                    <span className="text-gray-400">2024</span>
+                  </div>
+                  <p className={`text-white/65 tracking-tight font-[400] text-[0.8rem] ${roboto.className}`}>
+                    A minimal, terminal based solana CLI Wallet.
                   </p>
                 </div>
               </div>
@@ -139,7 +244,6 @@ export default function Home() {
         </section>
       </div>
 
-      {/* Background gradient */}
       <div className="fixed inset-0 -z-10 gradient-blur" />
     </main>
   )
--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -19,10 +19,10 @@ const projects = {
     title: 'Belzin',
     year: '2025',
     overview: 'AI-Agent Powered P2P Group Chat Betting platform built on Solana. The project leverages machine learning to provide intelligent betting suggestions and risk assessment.',
-    'live': 'https://belzin.vercel.app',
+    'live': 'https://belzin.fun',
     'url' : 'https://github.com/Shiva953/Belzin',
-    image: '',
-    tools: ['Next.js', 'TypeScript', 'Rust', 'Prisma', 'PostgreSQL']
+    image: '/belzin.png',
+    tools: ['Next.js', 'TypeScript', 'Rust', 'Prisma', 'PostgreSQL', 'solana/web3.js', 'Solana Wallet Adapter', 'Solana Actions']
   },
   'univault': {
     title: 'Univault',
@@ -30,17 +30,17 @@ const projects = {
     'live': 'https://univault.xyz',
     'url' : 'https://github.com/Shiva953/Univault',
     overview: 'Squads Multisig Management using Solana Blinks, providing a seamless and secure way to manage multi-signature wallets with enhanced UX.',
-    image: '',
+    image: '/univault.png',
     tools: ['Typescript', 'NextJS', 'Solana Actions', 'Blinks']
   },
   'liquotic': {
     title: 'Liquotic',
     year: '2024',
-    'live': 'https://liquotic.vercel.app',
+    'live': 'https://liquotic.xyz',
     'url' : 'https://github.com/Shiva953/Liquotic',
     overview: 'Liquotic lets you buy an NFT with any SPL-token(like USDC) rather than only SOL.',
-    image: '',
-    tools: ['Typescript', 'NextJS', 'Solana Actions', 'Prisma', 'PostgresSQL']
+    image: '/liquotic.jpg',
+    tools: ['Typescript', 'NextJS', 'Solana Actions', 'Prisma', 'PostgresSQL', 'Solana Wallet Adapter', 'solana/web3.js']
   },
   'openvest': {
     title: 'Openvest',
@@ -48,13 +48,14 @@ const projects = {
     'live': 'https://openvest.vercel.app',
     'url' : 'https://github.com/Shiva953/openvest',
     overview: 'Lets you create token vesting schedules for your company employees. Lock the supply into the company vault, and then choose the vest duration per choice.',
-    image: '',
+    image: '/openvest.png',
     tools: ['Typescript', 'NextJS', 'Anchor', 'Rust']
   },
 }
 
 export default async function ProjectPage({ params }: { params: { slug: Promise<string> } }) {
-  const slugF = await params.slug
+  const { slug } = await params;
+  const slugF = await slug;
   const project = projects[slugF as keyof typeof projects]
 
   if (!project) {
@@ -104,12 +105,14 @@ export default async function ProjectPage({ params }: { params: { slug: Promise<
         <div className="grid md:grid-cols-[1fr,1fr] gap-12">
           {/* Left Column - Image */}
           <div className="relative h-[300px] rounded-lg overflow-hidden">
+            <Link href={project.live}>
             <Image
-              src={project.image || "/placeholder.svg"}
+              src={project.image}
               alt={project.title}
               fill
-              className="object-cover"
+              className="object-cover cursor-pointer image-glow opacity-80 hover:opacity-100 transition-opacity"
             />
+            </Link>
           </div>
 
           {/* Right Column - Content */}
@@ -117,22 +120,25 @@ export default async function ProjectPage({ params }: { params: { slug: Promise<
             {/* Overview Section */}
             <div>
               <h2 className={`text-xl font-bold mb-4 ${supply.className}`}>Overview</h2>
-              <div className={`prose prose-invert max-w-none ${montreal.className}`}>
-                <p className="text-white/70 font-bold tracking-wide text-lg leading-relaxed">
+              <div className={`prose prose-invert max-w-[5rem] ${montreal.className}`}>
+                <p className="text-white/70 font-[600] tracking-[0.025rem] text-[17px] leading-[28px]">
                 {project.title == 'Belzin' && (
                     <>
                       AI-Agent Powered P2P Group Chat Betting platform built using the solana agent kit. Received a $3000 Grant from the <span className='text-white underline font-extrabold'>Solana Foundation</span>.
+
+                      {`\n`}
+                      You can find the contracts for the dapp <span className='text-white underline font-extrabold'><Link href={"https://github.com/Shiva953/belzin-betting-program"}>here</Link></span>.
                     </>
                   )}
 
                   {project.title == 'Univault' && (
                     <>
-                      All-In-One Squads Multisig Management using solana blinks. Send Vault Transactions and Vote on them, directly through a blink. <span className='text-white underline hover:cursor-pointer'>Blinkathon 2024 Winner</span> under Squads Track
+                      All-In-One Squads Multisig Management using solana blinks. Send Vault Transactions and Vote on them, directly through a blink. <span className='text-white underline hover:cursor-pointer'><Link href={"https://x.com/thesendcoin/status/1839324398102409634"}>Won the Blinkathon</Link></span> under Squads Track, a blinks-centred <span className='text-white underline hover:cursor-pointer'><Link href={"https://blinkathon.fun"}>solana hackathon</Link></span> organized by the send community.
                     </>
                   )}
                   {project.title == 'Liquotic' && (
                     <>
-                      Liquotic lets you buy an NFT with any SPL-token(like USDC) rather than only SOL.
+                      Liquotic lets you buy an NFT with any SPL token listed on the jup list, rather than only SOL. It combines jupiter swap API along with the Magiceden API, where the jupiter aggregator swaps the selected token to SOL, which is then used to buy the NFT.
                     </>
                   )}
                   {project.title == 'Openvest' && (
@@ -158,14 +164,11 @@ export default async function ProjectPage({ params }: { params: { slug: Promise<
                 ))}
               </div>
             </div>
-
-            {/* Timeline Section */}
            
           </div>
         </div>
       </div>
 
-      {/* Background gradient */}
       <div className="fixed inset-0 -z-10 gradient-blur" />
     </main>
   )
